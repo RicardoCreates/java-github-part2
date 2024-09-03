b87638a8f6f63541a241c76b275080e46566ba59
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-09-03 11:24:01 +0200

    Dont call a Button

--- src/main/java/de/ricardo/Main.java
+++ src/main/java/de/ricardo/Main.java
@@ -3,6 +3,7 @@ package de.ricardo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main branch!");
+        System.out.println("Dont call a Button");
         yellowButton();
     }
 
